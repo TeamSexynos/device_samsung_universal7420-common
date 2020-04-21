8e8c264d600dab49926493858b42474dcda4c9a2
Lunarixus <Nathanb644@hotmail.co.uk>
2020-04-21 15:29:47 +0000

    universal7420: Add missing shim libs
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -179,4 +179,7 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # Shim
 TARGET_LD_SHIM_LIBS += \
-    /system/bin/mediaserver|/vendor/lib/libstagefright_shim.so
+    /system/bin/mediaserver|/vendor/lib/libstagefright_shim.so \
+    /system/lib/libcamera_client.so|/vendor/lib/libcamera_client_shim.so \
+    /system/lib/libstagefright.so|/vendor/lib/libstagefright_shim.so \
+    /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so
