ac8b03bb475c8ce70be76baa356ac0ce4b580e95
Lunarixus <Nathanb644@hotmail.co.uk>
2020-04-21 13:42:58 +0000

    universal7420: Build basic USB HAL
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>

--- device-common.mk
+++ device-common.mk
@@ -173,6 +173,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # WiFi
 PRODUCT_PACKAGES += \
     wifiloader \
